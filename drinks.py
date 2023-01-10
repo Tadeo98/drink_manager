49516b453421351e242d60784a2eeae9388ecb0d
Tadeas <tadeas.cervik@gmail.com>
2023-01-10 18:40:14 +0100

    Simple_search

--- drinks.py
+++ drinks.py
@@ -1,3 +1,397 @@
 ### DRINK MANAGER BY Tadeo98
+import sys
 
-print('Welcome to Drink Manager!')
+def main(): #runs as first function from which other functions are triggered
+    width = 120  # should be divisible by the number of methods
+    methods = ['Search drink','Edit drink list','Edit list of ingredients']
+    print('\n\n\n{0}\n'.format((width-1)*'*'),end='')
+    print('{0:^{1}}\n'.format('WELCOME TO DRINK MANAGER!', width))
+    remain = 'y'
+    while remain == 'y':
+        print('{0:^{1}}\n'.format('Choose method (or type \'help\' for further actions).', width-1))
+        print_titles(width, methods, '|')
+        print_titles(width, range(1,len(methods)+1), '|')
+
+        method = in_method_control(list(range(1,len(methods)+1)),'\nEnter app number\n',width)
+
+        if method == 1:
+            search_drink(width)
+        elif method == 2:
+            edit_drink_list(width)
+        elif method == 3:
+            edit_ingredients(width)
+        else:
+            continue
+
+        remain = input('\n{0:^{1}}\n'.format('To remain in the program, enter [Y].', width)).lower()
+
+def search_drink(width):
+    methods = ['Search drink by name','Search drink by ingredients(s)','Search drink by available ingredients']
+    remain = 'y'
+    while remain == 'y':
+        print('\n\n{0}\n'.format((width-1) * '-'), end='')
+        print('{0:^{1}}\n'.format('SEARCH DRINK APPLICATION', width-1))
+        print_titles(width, methods, '|')
+        print_titles(width, range(1,len(methods)+1), '|')
+        print('\n',end='')
+        method = in_method_control(list(range(1,len(methods)+1)),'\nEnter app number\n',width)
+        if method == 1:
+            search_drink_by_name(width)
+        elif method == 2:
+            search_drink_by_ing(width)
+        elif method == 3:
+            search_drink_by_av_ing(width)
+        else:
+            continue
+
+        remain = input('\n{0:^{1}}\n'.format('To remain in the application, enter [Y].', width)).lower()
+
+def edit_drink_list(width): #function for editing the text file containing list of drinks
+    methods = ['Add drink','Edit drink','Delete drink']
+    remain = 'y'
+    while remain == 'y':
+        print('\n\n{0}\n'.format((width-1) * '-'), end='')
+        print('{0:^{1}}\n'.format('EDIT DRINK LIST APPLICATION', width-1))
+        print_titles(width, methods, '|')
+        print_titles(width, range(1,len(methods)+1), '|')
+        print('\n',end='')
+        method = in_method_control(list(range(1,len(methods)+1)),'\nEnter app number\n',width)
+        if method == 1:
+            add_drink(width)
+        elif method == 2:
+            edit_drink(width)
+        elif method == 3:
+            delete_drink(width)
+        else:
+            continue
+
+        remain = input('\n{0:^{1}}\n'.format('To remain in the application, enter [Y].', width)).lower()
+
+def edit_ingredients(width):
+    methods = ['Add ingredient','Edit ingredient','Delete ingredient']
+    remain = 'y'
+    while remain == 'y':
+        print('\n\n{0}\n'.format((width-1) * '-'), end='')
+        print('{0:^{1}}\n'.format('EDIT INGREDIENT LIST APPLICATION', width-1))
+        print_titles(width, methods, '|')
+        print_titles(width, range(1,len(methods)+1), '|')
+        print('\n',end='')
+        method = in_method_control(list(range(1,len(methods)+1)),'\nEnter app number\n',width)
+        if method == 1:
+            add_ingredient(width)
+        elif method == 2:
+            edit_ingredient(width)
+        elif method == 3:
+            delete_ingredient(width)
+        else:
+            continue
+
+        remain = input('\n{0:^{1}}\n'.format('To remain in the application, enter [Y].', width)).lower()
+
+def search_drink_by_name(width):    #searches drinks by name
+    print('\n\n{0}\n'.format((width - 1) * '.'), end='')
+    print('{0:^{1}}\n'.format('SEARCH DRINK BY NAME APPLICATION', width - 1))
+    while True:
+        print('Enter drink to search or type \'help\' for further actions:\n')
+        search = in_method_control('noint','Enter drink name (lowercase, capitals, doesn\'t matter)\n. Don\'t use '
+                            'symbols \';\' or \',\'.\nEnter any character to quit this application\n',width)
+        if search == None:
+            continue
+
+        if len(search) < 2:
+            break
+
+        forb = ';,'
+        if len(set(search) & set(forb)) > 0:
+            print('Forbidden symbols were used.\n', end='')
+            continue
+
+        search = search.lower()
+        drinks = read_drink_file()
+        met_drinks = {}
+        for name in drinks.keys():
+            if search in name.lower():
+                met_drinks.update({name: drinks.get(name)})
+        if met_drinks == {}:
+            print('No results.\n')
+            continue
+        drink_table(width,met_drinks)
+
+def search_drink_by_av_ing(width):  #searches all possible drinks by available ingredients
+    print('hi2')
+
+def search_drink_by_ing(width):  #searches all drinks containing entered ingredient(s)
+    print('hi2')
+
+def add_drink(width):   #subfunction for adding new drinks
+    print('\n\n{0}\n'.format((width - 1) * '.'), end='')
+    print('{0:^{1}}\n'.format('ADD DRINK TO LIST APPLICATION', width - 1))
+    while True:
+        print('Add new drink or type \'help\' for further actions:\n')
+        new_drink = in_method_control('noint','Format for new drink (one variation) input:\nDrink,'
+                    'Variation,ingredient 1,...,ingredient n,'
+                  'source\nFormat for new drink (more variations) input:\nDrink,Variation 1,ingredient 1,...,'
+                  'ingredient n,source;...;Variation n,ingredient 1,...,ingredient n,source\n'
+                                              'Enter any character to quit this application\n',width)
+        if new_drink == None:
+            continue
+
+        if len(new_drink) < 2:
+            break
+
+        if new_drink.count(',') < 3 or len(new_drink) < 7:
+            print('Incorrect format.\n',end='')
+            continue
+
+        new_drink = read_drink_row(new_drink)
+        drinks = read_drink_file()
+
+        if new_drink[0] in drinks.keys():
+            var_entered = set([var[0].lower() for var in new_drink[1:]])
+            var_list = set([var[0].lower() for var in drinks.get(new_drink[0])])
+            if len(var_entered & var_list) > 0:
+                print('Variation(s): {0} for {1} already exist(s). No change.\n'.format(', '.join
+                        (var_entered & var_list),new_drink[0]), end='')
+                continue
+            else:
+                file_lines = read_file_lines('drink_list.txt')
+                i = 0
+                for line in file_lines:
+                    line = line.strip('\n')
+                    if new_drink[0] == line.split(',',1)[0]:
+                        break
+                    i += 1
+                new_line = create_drink_line(file_lines[i],new_drink)
+                if new_line == None:
+                    continue
+                if file_lines[i] != file_lines[-1]:
+                    file_lines[i] = new_line + '\n'
+                else:
+                    file_lines[i] = new_line
+                edit_file_line(new_drink[0],file_lines,'drink_list.txt')
+                continue
+        new_line = create_drink_line('',new_drink)
+        if new_line == None:
+            continue
+        add_line_to_file(new_line,'drink_list.txt','drinks')
+
+
+def edit_drink(width):
+    print('hi')
+
+def delete_drink(width):
+    print('hi')
+
+def add_ingredient(width):  #function for adding new ingredients to list of ingredients
+    print('\n\n{0}\n'.format((width - 1) * '.'), end='')
+    print('{0:^{1}}\n'.format('ADD INGREDIENT TO LIST APPLICATION', width - 1))
+    while True:
+        print('Add new ingredient or type \'help\' for further actions:\n')
+        new_ing = in_method_control('noint','Enter ingredient.\nFor multiple word ingredient use \'_\' instead of '
+                    'space. No semicolons. No commas.\nEnter any character to quit this application\n',width)
+        if new_ing == None:
+            continue
+
+        if len(new_ing) < 2:
+            break
+
+        forb = ' ;,'
+        if len(set(new_ing) & set(forb)) > 0:
+            print('Forbidden symbols were used.\n',end='')
+            continue
+
+        new_ing = new_ing.lower()
+        ing_list = read_igredient_file()
+        if new_ing in ing_list.keys():
+            print('Ingredient {0} already exists.\n'.format(new_ing),end='')
+            continue
+        similar_ings = set()
+        for ing_part in new_ing.split('_'):
+            for ing in ing_list.keys():
+                if ing_part in ing:
+                    similar_ings.add(ing)
+        if len(similar_ings) > 0:
+            print('Similar ingredients already exist:\n{0}\nEnter \'N\' to cancel adding this '
+                  'ingredient\n'.format(', '.join(similar_ings)),end='')
+            if input().lower() == 'n':
+                continue
+        new_line = new_ing + ',' + str(len(ing_list.keys())+1)
+        add_line_to_file(new_line,'ingredients.txt','igredients')
+
+def edit_ingredient(width):
+    print('hi')
+
+def delete_ingredient(width):
+    print('hi')
+
+def drink_table(width,drinks):    #prints list of drinks
+    if drinks == {}:
+        drinks = read_drink_file()
+    title_len = max([len(name) for name in drinks.keys()])
+    var_len = max([len(var[0]) for drink in drinks.values() for var in drink])
+    #ing_len = max([len(', '.join(ing[1][0])) for ing in drinks.values()])
+    title = ['DRINK','VARIATION','INGREDIENTS']
+    mcs = 5 #minimal space between columns
+    print('\n\n{1:*<{0}}\n'.format(width-1,''),end='')
+    print("{1:^{0}}\n".format(width - 1, 'DRINK LIST'), end='')
+    print("{3:<{0}}{4:<{1}}{5:<{2}}\n\n\n".format(title_len+mcs,var_len+mcs, width-1-title_len-var_len,
+                                            title[0],title[1],title[2]),end='')
+    for drink, rest in drinks.items():
+        print("{1:<{0}}".format(title_len+mcs, drink),end='')
+        for var in rest:
+            print("{2:<{0}}{3:<{1}}\n".format(var_len + mcs, width - 1 - title_len - var_len, var[0],
+                                              ', '.join(var[1])), end='')
+            if var != rest[-1]:
+                print("{1:<{0}}".format(title_len+mcs,''), end='')
+            else:
+                print('\n',end='')
+    print('{1:*<{0}}\n'.format(width-1,''),end='')
+
+
+
+
+    var = drinks.get('Ramos Gin Fizz')[0]
+    ing_lines = []
+    ings = ', '.join(var[1])
+    index = ings[width - 3 - title_len - var_len].rindex(', ', 1)
+    ing_lines.append(ings[:index])
+    ings = ings[index:]
+    # index = ings.rindex(', ', width - 1 - title_len - var_len)
+    # ing_lines.append(ings[index:])
+    print(ing_lines, ings)
+
+
+def print_ingredients(width):   #prints list of ingredients
+    ings = read_igredient_file()
+    ing_len = max([len(name) for name in ings.keys()])
+    #num_len = max([len(str(num)) for num in ings.values()])
+    mcs = 2 #minimal space between columns
+    print('\n\n{1:*<{0}}\n'.format(width-1,''),end='')
+    print("{1:^{0}}\n\n".format(width - 1, 'LIST OF INGREDIENTS'), end='')
+    cols = (width-1)//(ing_len+mcs)
+    i = 1
+    for ing in ings.keys():
+        print("{1:<{0}}".format(ing_len+mcs,ing),end='')
+        if i % cols == 0:
+            print("{1:<{0}}\n".format(width-1-cols*ing_len,''),end='')
+        i += 1
+    print('\n\n{1:*<{0}}\n'.format(width-1,''),end='')
+
+def read_drink_file():  #reads drink file and returns dictionary of drinks
+    drink_list = read_file_lines('drink_list.txt')
+    drinks = {}
+    for drink in drink_list:
+        drink = drink.replace('\n', '')
+        variations1 = read_drink_row(drink)
+        drinks.update({variations1[0]: [[var[0], set(convert_ing([int(j) for j in var[1:-1]])),
+                                         var[-1]] for var in variations1[1:]]})
+    return drinks
+
+def read_drink_row(drink):  #reads one row of entered or file related drink and returns list format for one drink
+    drink = drink.split(';')
+    variations = []
+    for var in drink:
+        if var == drink[0]:
+            var = var.split(',')
+            variations.append(var[0])
+            variations.append(var[1:])
+            continue
+        variations.append(var.split(','))
+    return variations
+
+def print_titles(width, titles, symbol):    #prints rows with methods and corresponding numbers (entries)
+    for title in titles:
+        print('{1:^{0}}'.format(width // len(titles) - 1, title),end='')
+        if title != titles[-1]:
+            print(symbol,end='')
+    print('\n',end='')
+
+def create_drink_line(old_drink,drink):
+    if old_drink == '':
+        drink_line = drink[0] + ','
+    else:
+        drink_line = old_drink.strip('\n') + ';'
+    i = 1
+    for ings in [ing[1:-1] for ing in drink[1:]]:
+        conv_ings = convert_ing(ings)
+        if conv_ings == None:
+            break
+        drink_line += drink[i][0] + ',' + ','.join(str(num) for num in conv_ings) + ',' + drink[i][-1]
+        if i < len(drink[1:]):
+            drink_line += ';'
+        else:
+            return drink_line
+        i += 1
+
+def in_method_control(list_of_int,clause,width): #checks if entry is integer from interval of possible entries or help, drinkl, ingl
+    while True:
+        try:
+            value = input()
+            if value in ('help','ingl','drinkl'):
+                if value == 'help':
+                    print('{0}'.format(clause), end='')
+                    print_help()
+                elif value == 'drinkl':
+                    drink_table(width, {})
+                elif value == 'ingl':
+                    print_ingredients(width)
+                return None
+            if list_of_int == 'noint':
+                break
+            value = int(value)
+            if value in list_of_int:
+                break
+            else:
+                print('Enter {0} or {1}.'.format(', '.join(str(i) for i in list_of_int[:-1]),list_of_int[-1]))
+        except ValueError:
+            print('Enter integers only.')
+            return None
+    return value
+
+def convert_ing(conv_list):  #converts between numbers and ingredients according to ing list
+    ingredients = read_igredient_file()
+    ing_list = []
+    if type(conv_list[0]) == type(1):
+        for num in conv_list:
+            ing_list.append(list(ingredients.keys())[list(ingredients.values()).index(num)])
+    elif type(conv_list[0]) == type('1'):
+        missing = [miss for miss in conv_list if ingredients.get(miss) == None]
+        if len(missing) > 0:
+            print('List of ingredients does not contain {0}.\nEither adjust the format of ingredient(s) or '
+                  'add ingredient(s) to the list of ingredients.\n'.format(', '.join(missing)), end='')
+            return
+        for ing in conv_list:
+            ing_list.append(ingredients.get(ing))
+    return ing_list
+
+def read_igredient_file():  #returns dictionary of igredients
+    ing_list = read_file_lines('ingredients.txt')
+    ingredients = {}
+    for ing in ing_list:
+        ing = ing.replace('\n', '')
+        ing = ing.split(',')
+        ingredients.update({ing[0]:int(ing[1])})
+    return ingredients
+
+def add_line_to_file(line,path,item):
+    with open(path,'a') as f:
+        f.write('\n{0}'.format(line))
+    f.close()
+    print('{0} was added to list of {1}.\n'.format(line.split(',')[0],item),end='')
+
+def edit_file_line(drink,lines,path):
+    with open(path,'w') as f:
+        f.writelines(lines)
+    f.close()
+    print('{0} was edited in drink list.\n'.format(drink),end='')
+
+def read_file_lines(path):
+    with open(path,'r',encoding='utf8') as f:
+        lines = f.readlines()
+    return lines
+
+def print_help():   #prints help clause common for all methods
+    print('To see list of all drinks, type \'drinkl\'\nTo see list of all ingredients that can be used, type \'ingl\''
+          '\n\n', end='')
+
+main()
